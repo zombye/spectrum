b4cbf724d8eb1508cff3c179f4723b473bc55ff5
Zombye <y.zombye@gmail.com>
2017-11-16 15:06:09 +0100

    Halton sequence for temporal aa, gives a little less flickering

--- shaders/lib/misc/temporalAA.glsl
+++ shaders/lib/misc/temporalAA.glsl
@@ -1,26 +1,16 @@
+vec2 haltonSequence(vec2 i, vec2 b) {
+	vec2 f = vec2(1.0), r = vec2(0.0);
+	while (i.x > 0.0 || i.y > 0.0) {
+		f /= b;
+		r += f * mod(i, b);
+		i  = floor(i / b);
+	} return r;
+}
+
 vec2 taa_offset() {
 	vec2 scale = 2.0 / vec2(viewWidth, viewHeight);
 
-	const vec2[16] frameOffsets = vec2[16](
-		vec2(0.5, 0.5) * 0.25, // 1
-		vec2(2.5, 2.5) * 0.25, // 11
-		vec2(0.5, 2.5) * 0.25, // 9
-		vec2(2.5, 0.5) * 0.25, // 3
-		vec2(1.5, 1.5) * 0.25, // 6
-		vec2(3.5, 3.5) * 0.25, // 16
-		vec2(1.5, 3.5) * 0.25, // 14
-		vec2(3.5, 1.5) * 0.25, // 8
-		vec2(0.5, 1.5) * 0.25, // 5
-		vec2(2.5, 3.5) * 0.25, // 15
-		vec2(0.5, 3.5) * 0.25, // 13
-		vec2(2.5, 1.5) * 0.25, // 7
-		vec2(1.5, 0.5) * 0.25, // 2
-		vec2(3.5, 2.5) * 0.25, // 12
-		vec2(1.5, 2.5) * 0.25, // 10
-		vec2(3.5, 0.5) * 0.25  // 4
-	);
-
-	return frameOffsets[frameCounter % 16] * scale + (-0.5 * scale);
+	return haltonSequence(vec2(frameCounter % 16), vec2(2.0, 3.0)) * scale + (-0.5 * scale);
 }
 
 #if STAGE == STAGE_FRAGMENT
