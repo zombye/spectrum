7a3955fe7125d9c03e34c41023a00b772dc15e06
Jacob Eriksson <y.zombye@gmail.com>
2019-06-19 09:12:41 +0200

    Optimized rotation functions

--- shaders/lib/utility/rotation.glsl
+++ shaders/lib/utility/rotation.glsl
@@ -2,29 +2,34 @@
 #define INCLUDE_UTILITY_ROTATION
 
 vec2 Rotate(vec2 vector, float angle) {
-	vec2 sc = SinCos(angle);
-	return vec2(sc.y * vector.x + sc.x * vector.y, sc.y * vector.y - sc.x * vector.x);
+	float cosine = cos(angle);
+	float sine = sin(angle);
+
+	return vec2(cosine * vector.x - sine * vector.y, cosine * vector.y + sine * vector.x);
 }
 vec3 Rotate(vec3 vector, vec3 axis, float angle) {
 	// https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-	vec2 sc = SinCos(angle);
-	return sc.y * vector + sc.x * cross(axis, vector) + (1.0 - sc.y) * dot(axis, vector) * axis;
-}
+	float cosine = cos(angle);
+	float sine = sin(angle);
 
+	float tmp = dot(axis, vector);
+	return cosine * vector + sine * cross(axis, vector) + (tmp - tmp * cosine) * axis;
+}
 vec3 Rotate(vec3 vector, vec3 from, vec3 to) {
 	// where "from" and "to" are two unit vectors determining how far to rotate
 	// adapted version of https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
 
-	float cosTheta = dot(from, to);
-	vec3 axis = normalize(cross(from, to));
+	float cosine = dot(from, to);
+	vec3 axis = cross(from, to);
+	float cosecant = inversesqrt(dot(axis, axis));
 
-	vec2 sc = vec2(sqrt(1.0 - cosTheta * cosTheta), cosTheta);
-	return sc.y * vector + sc.x * cross(axis, vector) + (1.0 - sc.y) * dot(axis, vector) * axis;
+	return cosine * vector + cross(axis, vector) + (cosecant - cosecant * cosine) * cosecant * dot(axis, vector) * axis;
 }
 
 mat2 GetRotationMatrix(float angle) {
-	vec2 sc = SinCos(angle);
-	return mat2(sc.y, -sc.x, sc.x, sc.y);
+	float cosine = cos(angle);
+	float sine = sin(angle);
+	return mat2(cosine, -sine, sine, cosine);
 }
 
 #endif
